82c6c58586deaa6fafb740e67d4379fa2bb3881c
Ndg19 <158159787+Ndg19@users.noreply.github.com>
2025-09-12 18:53:16 +0300

    header and footer

--- netfflix-clone/src/App.jsx
+++ netfflix-clone/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import Home from './pages/Home/Home'
 
 function App() {
   
 
   return (
     <>
-     <div><h1>test</h1></div>
+     <Home />
     </>
   )
 }
---
+++ netfflix-clone/src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import Header from '../../components/Header/Header'
+import Footer from '../../components/Footer/Footer'
+
+
+const Home = () => {
+  return (
+    <>
+    <Header />
+    <Footer />
+    </>
+  )
+}
+
+export default Home
