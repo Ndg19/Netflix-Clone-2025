debe7abba432bc9dac92c2a4248bd58ebf846ad2
Ndg19 <158159787+Ndg19@users.noreply.github.com>
2025-09-14 19:35:38 +0300

    final commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// If you plan to deploy to GitHub Pages, set `base` to your repo name
 export default defineConfig({
   plugins: [react()],
-  base: "/Netflix-Clone-2025/",
+  base: "/Netflix-Clone-2025/", // **must match repo name exactly**
 });
