00c22e307be304c95d4b7c22f0e147292d16c451
Ndg19 <158159787+Ndg19@users.noreply.github.com>
2025-09-14 20:52:49 +0300

    final commit

--- netfflix-clone/vite.config.js
+++ netfflix-clone/vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Netflix-Clone-2025/netfflix-clone", // **must match repo name exactly**
+  base: "/Netflix-Clone-2025/", // **must match repo name exactly**
 });
