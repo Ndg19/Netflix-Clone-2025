a1c3ff1b50202c6596b8e45b191058243d62f8d8
Ndg19 <158159787+Ndg19@users.noreply.github.com>
2025-09-15 11:12:38 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   base: "/Netflix-Clone-2025/", // must match repo name exactly
+  build: {
+    outDir: "dist", // This is mandatory for gh-pages deployment
+  },
 });
